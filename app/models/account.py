4d2dc9b82b38dfc63e3a9f098c8abb504227fc1b
Joshua De Vera <deverajoshua02@gmail.com>
2025-06-12 10:36:18 -0500

    fixed error on creating account (no password are saved on the database)

--- app/models/account.py
+++ app/models/account.py
@@ -12,7 +12,8 @@ class Account(db.Model):
     user_id = db.Column(db.Integer)
     
     def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
+        self.password = generate_password_hash(password)
+
 
     def check_password(self, password):
-        return check_password_hash(self.password_hash, password)
+        return check_password_hash(self.password, password)
--- app/models/user.py
+++ app/models/user.py
@@ -6,6 +6,8 @@ class User(db.Model):
 
     
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
+    first_name = db.Column(db.String(255), unique=True, nullable=False)
+    last_name = db.Column(db.String(500), nullable=False)
+    birthdate = db.Column(db.DateTime)
+
     # notes = db.relationship('Note', backref='owner', lazy=True)
